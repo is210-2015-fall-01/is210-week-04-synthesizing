4d97b051669118d5d242e7b96562e975b2094be5
Shinseeker <dailymanj88@live.com>
2015-09-18 03:41:19 +0000

    fixed spacining again for task 1-3

--- task_01.py
+++ task_01.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""converting a Fahrenheit temperature to a Kelvin Task 1-3"""
-
+"""Converting a Fahrenheit temperature to Kelvin task 1-3."""
 
 import decimal
 
@@ -9,36 +8,33 @@ ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
 
 
 def fahrenheit_to_celsius(degrees):
-    """Performs arithmetic to convert temperature from fahrenheit to celsius.
+    """A function to convert fahrenheit to celsius.
+
+    The input will be converted from fahrenheit to celcious and returned
+    as a decimal.
 
     Args:
-        degrees (int): Arg to be subtracted, multiplied and divided.
-
+       degrees (int): A value in fahrenheit.
     Returns:
-        int: Arg converted to another number arithmetically.
+        Returns the temperature in celsius as a decimal.
+    Example:
 
-    Examples:
+    >>> fahrenheit_to_celsius(212)
+     Decimal('100')
 
-        >>> fahrenheit_to_celsius(212)
-        Decimal('100')
     """
-    return (degrees - 32) * 5 / decimal.Decimal('9')
+    return ((decimal.Decimal(degrees) - 32) * 5) / 9
 
 
 def celsius_to_kelvin(degrees):
     """Performs arithmetic to convert temperature from celsius to kelvin.
-
     Args:
         degrees (int): Arg to be added to ABSOLUTE DIFFERENCE.
-
     Returns:
         int: Arg converted to another value arithmetically.
-
     Examples:
-
         >>> celsius_to_kelvin(100)
         Decimal('373.15')
-
         >>> celsius_to_kelvin(50)
         Decimal('323.15')
     """
@@ -47,20 +43,16 @@ def celsius_to_kelvin(degrees):
 
 def fahrenheit_to_kelvin(degrees):
     """Performs arithmetic to convert temperature from fahrenheit to kelvin.
-
     Args:
         degrees (int): Arg to be converted another in through arithmetic, then
         converted again by adding to ABSOLUTE_DIFFERENCE.
-
     Returns:
         int: Arg converted to another value arithmetically.
-
     Examples:
-
         >>> fahrenheit_to_kelvin(212)
         Decimal('373.15')
-
         >>> fahrenheit_to_kelvin(72)
         Decimal('295.3722222222222222222222222')
     """
+
     return celsius_to_kelvin(fahrenheit_to_celsius(degrees))
