ccd9060621536eb6b60f7428c4c260ecd8bf7c60
Alex Doler <aedoler@gmail.com>
2015-09-19 15:48:32 +0000

    work in progress

--- task_01.py
+++ task_01.py
@@ -1,33 +1,35 @@
 #!user/bin/env python
 # -*- coding: utf-8 -*-
-"""Module that tests to see if there are enough litterboxes"""
+"""Module that converts fahrenheit to celsius"""
 
 import decimal
 
 def fahrenheit_to_celsius(degrees):
-    """ Returns two integers and boolean to ensure that there enough litterboxes
-        for all of the cats.
+    """ Returns conversion from f to c as a decimal.
 
     Args:
-        kittens (int): Number of kittens.
-        litterboxes (int): Number of litterboxes.
-        cafood (bool): At least some catfood? True or False.
+        kittens (dec): degree to be converted
 
     Returns:
-        A bool value indicating whether or not there are enough litterboxes
-        for all of the kittens and at least some catfood.
+        (dec) converted to celsius
 
     Examples:
-         too_many_kittens(4, 5, True)
+         
     """
-   
-    degree_val = ((degrees - decimal.Decimal('32.0')) * decimal.Decimal('5') /
-                  decimal.Decimal('9'))
 
-    return degree_val
+ 
 
-decimal1 = decimal.Decimal('95')
+    a = decimal.Decimal('32')
+    b = decimal.Decimal('5')
+    c = decimal.Decimal('9')
+    return ((degrees - a) * b / c)
 
-f_to_c = fahrenheit_to_celsius(decimal1)
+DEGREE_VAL = decimal.Decimal('51')
 
-print f_to_c
+print fahrenheit_to_celsius(DEGREE_VAL)
+
+
+
+
+
+
