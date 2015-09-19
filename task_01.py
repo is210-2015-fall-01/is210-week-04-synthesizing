1f2b391620abc5938faeac87718ab2f1a51067f1
Alex Doler <aedoler@gmail.com>
2015-09-19 17:04:47 +0000

    Finally got code to pass unit test

--- task_01.py
+++ task_01.py
@@ -2,31 +2,31 @@
 # -*- coding: utf-8 -*-
 """Module that converts fahrenheit to celsius"""
 
+
 import decimal
 
-def fahrenheit_to_celsius(degrees=decimal.Decimal('53')):
+
+def fahrenheit_to_celsius(degrees):
     """ Returns conversion from f to c as a decimal.
 
     Args:
-        kittens (dec): degree to be converted
+        degrees (dec): degree to be converted
 
     Returns:
         (dec) converted to celsius
 
     Examples:
-         
+        fahrenheit_to_celsius(53)
+        >>>> '11.66666666666666666666666667'
     """
-    
-    a = decimal.Decimal('32')
-    b = decimal.Decimal('5')
-    c = decimal.Decimal('9')
-    return (str(degrees - a) * b / c)
+    deg_far = decimal.Decimal(degrees)
+    val_a = decimal.Decimal('32')
+    val_b = decimal.Decimal('5')
+    val_c = decimal.Decimal('9')
+
+    dec_val = ((deg_far - val_a) * val_b) / val_c
+
+    return dec_val
 
 
-print fahrenheit_to_celsius()
-
-
-
-
-
-
+print fahrenheit_to_celsius(53)
