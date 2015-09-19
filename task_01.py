5b06ac50b74cd0df74c8b595bef8084dc8ef655e
Grace Park <pjeanne14@gmail.com>
2015-09-19 17:09:52 +0000

    Just complete Task_02. Changed to Kelvin.

--- task_01.py
+++ task_01.py
@@ -3,11 +3,15 @@
 """This module will convert Fahrenheit to Celsius."""
 
 
+from decimal import Decimal
+
+ABSOLUTE_DIFFERENCE = Decimal('273.15')
+
 def fahrenheit_to_celsius(degrees):
     """This will change the degrees to C from F.
 
     Args:
-        degrees(float):the temperature in degrees F.
+        degrees(int):the temperature in degrees F.
 
     Returns:
         decimal: decimal representation in degrees C.
@@ -16,4 +20,19 @@ def fahrenheit_to_celsius(degrees):
         fahrenheit_to_celsius(212)
         'Decimal (100.0)'
     """
-    return 'Decimal ({})'.format((5.0000*((degrees * 1.0000)-32.0000))/9.0000, '.3f')
+    return 'Decimal ({})'.format((5.0000*((degrees)-32.0000))/9.0000, '.3f')
+
+def celsius_to_kelvin(degrees):
+    """This will change the degrees to K from C.
+
+    Args:
+        degrees(decimal):the temperature in degrees C(decimal form).
+
+    Returns:
+        decimal: decimal representation in degrees K.
+
+    Examples:
+        celsius_to_kelvin(212)
+        'Decimal (100.0)'
+    """
+    return 'Decimal ({})'.format(ABSOLUTE_DIFFERENCE+degrees, '.3f')
