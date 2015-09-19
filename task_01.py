7e4e8a12ee70dae393096543640be7964dd32525
Alex Doler <aedoler@gmail.com>
2015-09-19 17:50:04 +0000

    completed task 2, added to same file as task 1

--- task_01.py
+++ task_01.py
@@ -6,14 +6,17 @@
 import decimal
 
 
+ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
+
+
 def fahrenheit_to_celsius(degrees):
-    """ Returns conversion from f to c as a decimal.
+    """Does math to return conversion from f to c as a decimal.
 
     Args:
-        degrees (dec): degree to be converted
+        degrees (dec): degree to be converted.
 
     Returns:
-        (dec) converted to celsius
+        (dec) converted to celsius.
 
     Examples:
         fahrenheit_to_celsius(53)
@@ -28,5 +31,26 @@ def fahrenheit_to_celsius(degrees):
 
     return dec_val
 
+
+def celsius_to_kelvin(degrees):
+    """"Does math to convert celsius to kelvin.
+
+    Args:
+        degrees (dec): degree in celsius to be converted.
+
+    Returns:
+        (dec) converted to kelvin.
+
+    Examples:
+        celsius_to_kelvin(100)
+        >>> '373.15'
+    """
+    deg_cel = decimal.Decimal(degrees)
+    val_kelv = deg_cel + ABSOLUTE_DIFFERENCE
+
+    return val_kelv
+
+
+print celsius_to_kelvin(100)
 
 print fahrenheit_to_celsius(53)
