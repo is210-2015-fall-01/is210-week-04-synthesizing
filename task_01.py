8eca07b924a57fc06a792b52381d6a14e4e15597
Grace Park <pjeanne14@gmail.com>
2015-09-20 20:21:15 +0000

    Made sure to use decimal.Decimal. Corrected output to have the single quotes inside ().

--- task_01.py
+++ task_01.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module will convert Fahrenheit to Celsius."""
+"""This module will convert Fahrenheit to Celsius.
+There is also a conversion from C to K and F directly to K."""
+
+import decimal
 
 
-from decimal import Decimal
-
-
-ABSOLUTE_DIFFERENCE = 273.15
+ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
 
 
 def fahrenheit_to_celsius(degrees):
-    """This will change the degrees to C from F.
+    """This will change the degrees to Celsius from Fahrenheit.
 
     Args:
         degrees(int):the temperature in degrees F.
@@ -19,46 +19,45 @@ def fahrenheit_to_celsius(degrees):
         decimal: decimal representation in degrees C.
 
     Examples:
-    
+
         >>>fahrenheit_to_celsius(212)
-        'Decimal (100.0)'
+        Decimal ('100.0')
     """
-    return 'Decimal ({})'.format((5.0000*((Decimal.degrees(degrees, '.2f'))-32.0000))/9.0000, '.3f')
+    return (5*(decimal.Decimal(degrees)-32))/9
 
 
 def celsius_to_kelvin(degrees):
-    """This will change the degrees to K from C.
+    """This will change the degrees to Kelvin from Celsius.
 
     Args:
         degrees(decimal):the temperature in degrees C.
 
     Returns:
-        decimal: decimal representation in degrees K.
+        decimal: decimal representation of temperature in degrees K.
 
     Examples:
-    
-        >>>celsius_to_kelvin(212)
-        'Decimal (100.0)'
+
+        >>>celsius_to_kelvin(100)
+        Decimal ('373.15')
+        >>>celsius_to_kelvin(200)
+        Decimal ('473.15')
     """
-    int1=int(ABSOLUTE_DIFFERENCE)
-    return 'Decimal ({})'.format((int1*1.000)+degrees, '.3f')
+    return decimal.Decimal(degrees) + ABSOLUTE_DIFFERENCE
 
 
 def fahrenheit_to_kelvin(degrees):
 
-    """This will change the degrees to K from F.
+    """This will change the degrees to Kelvin from Fahrenheit.
 
     Args:
-        degrees(int):the temperature in degrees in F.
+        degrees(decimal):the temperature in degrees in F.
 
     Returns:
         decimal: decimal representation in degrees K.
 
     Examples:
-    
-        >>>fahrenheit_to_kelvin(212)
-        'Decimal (373.15)'
-    """
-    return 'Decimal ({})'.format(celsius_to_kelvin(fahrenheit_to_celsius(degrees)), '.3f')
-print fahrenheit_to_kelvin(212)
 
+        >>>fahrenheit_to_kelvin(212)
+        Decimal ('373.15')
+    """
+    return celsius_to_kelvin(fahrenheit_to_celsius(degrees))
